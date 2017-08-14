f3e4e9429a2e0cd7a3d28b9223ff335439914a6b
NileshGule <vn_nilesh@yahoo.com>
2017-08-14 21:21:42 +0800

    temporarily disabled test

--- src/test/java/com/nileshgule/PairRDDExampleTest.java
+++ src/test/java/com/nileshgule/PairRDDExampleTest.java
@@ -1,10 +1,12 @@
 package com.nileshgule;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 
 public class PairRDDExampleTest {
 
+    @Disabled
     @Test
     void testMain() {
         String[] args = null;
